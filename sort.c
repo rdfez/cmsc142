f0d984e12a03ed5c406de8b0c66f931709d46590
rdfez <rdfernandez3@up.edu.ph>
2024-10-23 13:51:44 +0800

    Update sort.c

--- sort.c
+++ sort.c
@@ -10,54 +10,51 @@ void swap(int *a, int *b){
     *b=temp;
 }
 
-// input_case = [asc, desc, random]
-// random = [rand1, rand2, rand3] (NULL for asc, desc)
-void init(int a[], int n, int input_case, int random){
-    int i;
-
-    switch(input_case) {
-        case 0:
-            // ascending
-            for(i=0;i<n;i++)
-                a[i]=i+1;
-            break;
-        case 1:
-             // descending
-            for(i=0;i<n;i++)
-                a[i]=n-i;
-            break;
-        case 2:
-            // random
-            // initialize array a
-            for(i=0;i<n;i++)
-                a[i]=i+1;
-            
-            // set seed
-            switch(random){
-                case 0:
-                    srand(7);
-                    break;
-                case 1:
-                    srand(13);
-                    break;
-                case 2:
-                    srand(17);
-                    break;
-            }
-            
-            for(i=0;i<n;i++)
-                swap(&a[i],&a[rand()%n]);
-    }
-    
-}
-
 // print array
 void output(int a[], int n){
     int i;
-    
+
     for(i=0;i<n;i++)
-        printf("%i\n", a[i]);
+        printf("%i ", a[i]);
     printf("\n");
+}
+
+// input_case = [asc, desc, random]
+// random = [rand1, rand2, rand3] (NULL for asc, desc)
+void init(int a[], int n, char *input_case, int random){
+    int i;
+    
+    // ascending
+    if (!strcmp(input_case, "asc")) { 
+        for(i=0;i<n;i++)
+            a[i]=i+1;
+    // descending
+    } else if (!strcmp(input_case, "desc")) {
+        for(i=0;i<n;i++)
+            a[i]=n-i;
+    // random
+    } else if (!strcmp(input_case, "rand")) { 
+        // random
+        // initialize array a
+        for(i=0;i<n;i++)
+            a[i]=i+1;
+        
+        // set seed
+        switch(random){
+            case 0:
+                srand(7);
+                break;
+            case 1:
+                srand(13);
+                break;
+            case 2:
+                srand(17);
+                break;
+        }
+        
+        for(i=0;i<n;i++)
+            swap(&a[i],&a[rand()%n]);
+    }
 }
 
 // insertion sort
@@ -111,7 +108,7 @@ void bsort(int a[], int n){
             if(a[i]>a[i+1]) {
                 swap(&a[i], &a[i+1]);
                 swapped = 1;
-            } else break;
+            }
     }
 }
 
@@ -121,12 +118,13 @@ void ssort(int a[], int n){
     int min_index;
 
     for(i=0;i<n-1;i++) {
-        min_index = a[i];
-        for(j=i; j<n-1;j++)
-            if(a[j]>a[j+1]) {
-                min_index = j+1;
-            } else break;
-        swap(&a[i], &a[min_index]);
+        min_index = i;
+        for(j=i+1; j<n;j++)
+            if(a[j]<a[min_index]) {
+                min_index = j;
+            }
+        if (min_index != i)
+            swap(&a[i], &a[min_index]);
     }
 }
 
@@ -160,12 +158,50 @@ int compare(const void *a, const void *b) {
     return (*(int*)a - *(int*)b);
 }
 
+// for heap sort
+void heapify(int arr[], int n, int i) {
+    // indices for root, left child and right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    // Find largest among root, left child and right child
+    // if left is within bounds and greater than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    // if right is within bounds and greater than root
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
 // heap sort
+void hsort(int arr[], int n) {
+    // Build max heap
+    // i = non-leaf nodes (from n/2-1)
+    for (int i=n/2-1; i>=0; i--)
+        heapify(arr, n, i);
+  
+    // Heap sort
+    for (int i=n-1; i>=0; i--) {
+        // swap root (largest) with last element
+        swap(&arr[0], &arr[i]);
+    
+        // Heapify root element to get highest element at root again
+        heapify(arr, i, 0);
+    }
+}
 
 int main(int argc,char *argv[]){
     char *type = argv[1];
     int size = atoi(argv[2]);
-    char *sort_algo = argv[2]; 
+    char *sort_algo = argv[3];
+
     // int n=10;
     int n=10000*(size);
     int a[n], i;
@@ -174,44 +210,43 @@ int main(int argc,char *argv[]){
     // init(a,n,<input_case>,<random>)
     // @ <input_case> = [asc, desc, random]
     // @ <random> = seed no. = [rand1, rand2, rand3]
-    
     // ascending
-    if (type == "0")
-        init(a,n,0,0);
+    if (!strcmp(type, "asc"))
+        init(a,n,"asc",NULL);
     // descending
-    else if (type == "1") 
-        init(a,n,1,0);
+    else if (!strcmp(type, "desc"))
+        init(a,n,"desc",NULL);
     // random1
-    else if (type == "2") 
-        init(a,n,2,0);
+    else if (!strcmp(type, "rand1"))
+        init(a,n,"rand",0);
     // random2
-    else if (type == "3") 
-        init(a,n,2,1);
+    else if (!strcmp(type, "rand2"))
+        init(a,n,"rand",1);
     // random3
-    else if (type == "4") 
-        init(a,n,2,2);
-
+    else if (!strcmp(type, "rand3")) 
+        init(a,n,"rand",2);
+    
     // output(a,n);
-
     t1 = clock();
-    // sorting algorithms
 
-    if (sort_algo == "isort")
+    // sort
+    if (!strcmp(sort_algo,"isort"))
         isort(a,n);
-    else if (sort_algo == "shsort")
+    else if (!strcmp(sort_algo, "shsort"))
         shsort(a,n);
-    else if (sort_algo == "msort")
+    else if (!strcmp(sort_algo, "msort"))
         msort(a,n);
-    else if (sort_algo == "bsort")
+    else if (!strcmp(sort_algo, "bsort"))
         bsort(a,n);
-    else if (sort_algo == "ssort")
+    else if (!strcmp(sort_algo, "ssort"))
         ssort(a,n);
-    else if (sort_algo == "qsort")
+    else if (!strcmp(sort_algo, "qsort"))
         qsort(a, n, sizeof(int), compare);
-    else if (sort_algo == "qsort1")
+    else if (!strcmp(sort_algo, "qsort1"))
         qsort1(a,0,n-1);
-    else if (sort_algo == "hsort")
-        qsort1(a,0,n-1);
+    else if (!strcmp(sort_algo, "hsort"))
+        hsort(a,n);
+    
     t2 = clock();
 
     // output(a,n);
